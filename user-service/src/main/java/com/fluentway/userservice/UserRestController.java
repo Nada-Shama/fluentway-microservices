e763ee06af2de3c516f8c5b592e68a79b32c62d8
Nada-Shama <nada.h.shamaa@gmail.com>
2025-06-28 18:13:29 +0300

    Update UserRestController.java

--- user-service/src/main/java/com/fluentway/userservice/UserRestController.java
+++ user-service/src/main/java/com/fluentway/userservice/UserRestController.java
@@ -4,28 +4,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/users")
-@CrossOrigin(origins = "*")
 public class UserRestController {
 
-    private final List<Map<String, String>> users = new ArrayList<>(List.of(
+    private List<Map<String, String>> users = new ArrayList<>(List.of(
         Map.of("id", "1", "name", "Nada"),
         Map.of("id", "2", "name", "Ali"),
         Map.of("id", "3", "name", "Sara")
     ));
 
-    @GetMapping
+    @GetMapping("/users")
     public List<Map<String, String>> getUsers() {
         return users;
     }
 
-    @PostMapping
+    @PostMapping("/users")
     public Map<String, String> addUser(@RequestBody Map<String, String> user) {
-        if (user.containsKey("id") && user.containsKey("name")) {
-            users.add(user);
-            return user;
-        } else {
-            return Map.of("error", "Invalid user format");
-        }
+        users.add(user);
+        return user;
     }
 }
