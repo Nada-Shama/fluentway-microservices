2aac2d1a5c3360da7fd0272e0a02f2944bb4e768
Nada-Shama <nada.h.shamaa@gmail.com>
2025-06-28 17:27:40 +0300

    Update UserViewController.java

--- user-service/src/main/java/com/fluentway/userservice/UserViewController.java
+++ user-service/src/main/java/com/fluentway/userservice/UserViewController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserViewController {
+
     @GetMapping("/")
-    public String home() {
-        return "ui"; // renders ui.html
+    public String showUI() {
+        return "ui";
     }
 }
